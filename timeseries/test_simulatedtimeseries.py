ef49c60d6cae20587c7510cae610d888f22f0c76
nickruta <nickruta@gmail.com>
2016-10-31 21:52:26 -0400

    added produce test to get 100percent coverage for simulatedtimeseries

--- timeseries/test_simulatedtimeseries.py
+++ timeseries/test_simulatedtimeseries.py
@@ -3,17 +3,11 @@ from simulatedtimeseries import SimulatedTimeSeries
 import numpy as np
 
 def test_chunk_out_of_range():
-
-	with raises(StopIteration):
-
-		chunk = 200
-
-		test_range = range(chunk)
-
-		times_gen = (t for t in [1,2,3])
-		values_gen = (v for v in [1,2,3])
-		ts = SimulatedTimeSeries(times_gen, values_gen)
-		produced = [(next(ts.times), next(ts.values)) for _ in range(chunk)]
+	with raises(IndexError):
+		t = iter(range(1,11))
+		v = iter([2*x + 1 for x in range(1,11)])
+		sts = SimulatedTimeSeries(t, v)
+		sts.produce(15) == [(1,3), (2,5), (3,7)]
 
 def test_online_mean():
 	times_gen = (t for t in [1,2,3,4])
