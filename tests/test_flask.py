6969c53faee93b2cfa7ff902004bcfbf2d77810d
Jonne Saleva <jonne.saleva@gmail.com>
2016-12-13 20:28:52 -0500

    edited test script to accept app module

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -8,15 +8,21 @@
 from flask import render_template, make_response, jsonify, request
 from cs207project.flask.helper_functions import *
 from cs207project.flask.app import app, db, models
-import flask_restful
-import re
 
 # test suite
 """
 Tests that should be run:
 - basic endpoint connectivity: 
-	- /timeseries/ works and returns metadata
-	- /timeseries/level_in returns subset of levels
-	- /timeseries/level_in fails gracefully if given nonexistent level (incl composite case)
-	- /timeseries/level_in fails gracefully if given no input (invalid url string)
+	- /timeseries/ GET
+		- /timeseries/ works and returns metadata
+		- /timeseries/level_in returns subset of levels
+		- /timeseries/level_in returns subset of levels
+		- /timeseries/level_in if given nonexistent level (incl composite case)
+		- /timeseries/level_in if given no input (invalid url string)
+		- /timeseries/level_in: non-letter inputs, wrong separator
+		- /timeseries/mean_in: non-numeric inputs, wrong separator
+		- /timeseries/mean_in: input not in sorted order
+	- /timeseries/ POST
+
+- 
 """
